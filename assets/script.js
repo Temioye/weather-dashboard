be8ba3a5adf8bc3a6519c5391a14e32b2a4c493d
temio <temioyelade8@gmail.com>
2023-03-24 14:58:30 +0000

    function for todays weather

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,27 @@ var city;
 var today = $("#today");
 var forecast = $("#forecast");
 var date = moment().format("MM/DD/YYYY");
+
+//function for today's weather
+function displayWeatherData(response) {
+  var celsiusTemp = "Temp: " + (response.main.temp - 273.15).toFixed(2) + "°C";
+  var windSpeed = "Wind: " + response.wind.speed + " KPH";
+  var humidity = "Humidity " + response.main.humidity + "%";
+  var todayCard = $("<div>").addClass("card bg-light border-dark");
+  var todayCardBody = $("<div>").addClass("card-body text-dark p-2");
+  var weatherIconUrl =
+    "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+  var iconContainer = $("<div id='weather-icon'>").html(
+    "<img src='" + weatherIconUrl + "' alt='Weather Icon'>"
+  );
+  var todayDateDisplay = $("<h5>").text(response.name + " (" + date + ")");
+  todayCardBody.append(todayDateDisplay);
+  todayCardBody.append(iconContainer);
+  todayCardBody.append("<p>" + celsiusTemp + "</p>");
+  todayCardBody.append("<p>" + windSpeed + "</p>");
+  todayCardBody.append("<p>" + "Humidity: " + humidity + "</p>");
+  todayCard.append(todayCardBody);
+  today.empty();
+  today.append(todayCard);
+  displayForecast(response.id);
+}
