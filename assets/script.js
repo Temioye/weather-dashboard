a3b82115fd68ce0136b3dc6847916b0eca4c2092
temio <temioyelade8@gmail.com>
2023-03-24 14:57:25 +0000

    added variables and apikey

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,7 @@
-
+//Variables
+var APIKey = "cd8542d18f73261622c8d59475ca57c0";
+var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+var city;
+var today = $("#today");
+var forecast = $("#forecast");
+var date = moment().format("MM/DD/YYYY");
